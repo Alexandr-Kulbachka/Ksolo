fd821e02a93ce28a3950d733de2f913889769426
Alexandr Kulbachka <alexandr.kulbachka@gmail.com>
2020-10-08 00:58:48 +0300

    Changed progect structure

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'pages/home.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return MultiProvider(
+    //   providers: [],
+    //   child: MaterialApp(
+    //     title: 'Flutter Demo',
+    //     theme: ThemeData(
+    //       primarySwatch: Colors.blue,
+    //       visualDensity: VisualDensity.adaptivePlatformDensity,
+    //     ),
+    //     home: MyHomePage(title: 'Flutter Demo Home Page'),
+    //   ),
+    // );
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:list_manager/app.dart';
 
 import 'package:list_manager/main.dart';
 
